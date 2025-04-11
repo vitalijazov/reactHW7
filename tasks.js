376451365607d941df995d2ed0123ae6632b6ac4
vitalijazov <151974129+vitalijazov@users.noreply.github.com>
2025-04-11 17:21:58 +0300

    Add files via upload

---
+++ tasks.js
@@ -0,0 +1,7 @@
+const tasks = [
+    { id: 1, title: 'Задача 1', completed: true },
+    { id: 2, title: 'Задача 2', completed: true },
+    { id: 3, title: 'Задача 3', completed: false },
+  ];
+  
+  export default tasks;
